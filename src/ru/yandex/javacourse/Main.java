480ac04edca17558b1de992161ec155aa5aedcc9
κρμγ <petryxa.clever@gmail.com>
2023-12-20 16:11:15 +0300

    Initial commit

---
+++ src/ru/yandex/javacourse/Main.java
@@ -0,0 +1,7 @@
+package ru.yandex.javacourse;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ test/ru/yandex/javacourse/MainTest.java
@@ -0,0 +1,11 @@
+package ru.yandex.javacourse;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void test(){
+
+    }
+}
