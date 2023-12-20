2417d5d364212888db5ce2212bf92d9f4212158f
kpmy <petryxa.clever@gmail.com>
2023-12-20 16:20:42 +0300

    i need tests

---
+++ src/ru/yandex/javacourse/ReversePolishNotationCalculator.java
@@ -0,0 +1,47 @@
+package ru.yandex.javacourse;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class ReversePolishNotationCalculator {
+
+    public int calculatePolishNotation(String str) {
+        String[] parts = str.split(" ");
+        Deque<Integer> numbers = new ArrayDeque<>();
+        int index = 0;
+
+        while (index != parts.length) {
+
+            if (parts[index].isBlank()) {
+                index++;
+                continue;
+            }
+
+            if (isOperation(parts[index])) {
+                int operandOne = numbers.pop();
+                int operandTwo = numbers.pop();
+
+                if (parts[index].equals("+")) {
+                    numbers.push(operandOne + operandTwo);
+                } else if (parts[index].equals("-")) {
+                    numbers.push(operandTwo - operandOne);
+                } else if (parts[index].equals("*")) {
+                    numbers.push(operandOne * operandTwo);
+                }
+
+            } else {
+                numbers.push(Integer.parseInt(parts[index]));
+            }
+
+            index++;
+        }
+
+        return numbers.pop();
+    }
+
+    private boolean isOperation(String part) {
+        return part.equals("+")
+                || part.equals("-")
+                || part.equals("*");
+    }
+}
---
+++ test/ru/yandex/javacourse/ReversePolishNotationCalculatorTest.java
@@ -0,0 +1,21 @@
+package ru.yandex.javacourse;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReversePolishNotationCalculatorTest {
+
+    private final ReversePolishNotationCalculator rpn = new ReversePolishNotationCalculator();
+
+    @Test
+    public void shouldCalculateAddition() {
+        //TODO
+    }
+
+    //TODO
+
+}
+
